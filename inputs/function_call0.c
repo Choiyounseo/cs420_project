329dc8c98fefb230cfa3d62dd4668d1d3bbd0879
Taehyun Hwang <taehyun95@gmail.com>
2019-12-12 13:10:25 +0900

    Arrange test cases for function call

--- inputs/function_call0.c
+++ inputs/function_call0.c
@@ -1,11 +1,34 @@
-void test(void) {
-    int b;
-    return 3;
+int identity(int a) {
+    return a;
 }
 
 int main(void) {
     int a;
-    a = 2;
-    test();
-    printf("test is pass");
+    a = identity(5);
+    printf ("Identity : ");
+    printf ("%d", a);
+    printf (", Excepted : ");
+    printf ("%d\n", 5);
+
+    int i;
+    int count = 5;
+    int sum, sum2;
+    sum = 0;
+    sum2 = 0;
+
+    for (i = 1; i < count; i ++) {
+        sum = sum + identity(i);
+        sum2 = sum2 + identity(i) + identity(i);
+    }
+
+    printf ("%d\n", sum);
+    printf ("%d\n", sum2);
+
+    if (sum2 > sum) {
+        printf ("It's correct!\n");
+    }
+
+    if (sum2 < sum) {
+        printf ("It's incorrect!\n");
+    }
 }
--- inputs/function_call1.c
+++ inputs/function_call1.c
@@ -1,12 +1,15 @@
-void test(void) {
-    int b;
-    return 3;
+int sum(int num) {
+	int num2;
+	num2 = 0;
+    if (num > 0) {
+        num2 = sum(num-1);
+        num2 = num2 + num;
+    }
+    return num2;
 }
 
 int main(void) {
     int a;
-    a = 2;
-    int c;
-    c = test();
-    printf("test is pass");
+    a = sum(5);
+    printf ("%d\n", a);
 }
