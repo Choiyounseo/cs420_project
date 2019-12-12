7389e9327b3f2f6a929abebce629757e3c07e26a
Taehyun Lee <etaehyun4@gmail.com>
2019-12-13 03:21:00 +0900

    Refactor yacc results
    - list to dict

--- cyacc.py
+++ cyacc.py
@@ -26,7 +26,14 @@ def p_func(p):
             | FLOAT ID LPAREN paramlist RPAREN lbrace stmtlist rbrace
             | VOID ID LPAREN paramlist RPAREN lbrace stmtlist rbrace
     '''
-    p[0] = ["function", p[1], p[2], ["parameter", p[4]], [p[6]] + p[7] + [p[8]], [p.lineno(1), p[8][1]]]
+#    p[0] = ["function", p[1], p[2], ["parameter", p[4]], [p[6]] + p[7] + [p[8]], [p.lineno(1), p[8][1]]]
+    p[0] = ["function", {
+                "type": p[1],
+                "name": p[2],
+                "params": p[4],
+                "stmts": [p[6]] + p[7] + [p[8]],
+                "lineno": [p.lineno(1), p[8][1]["lineno"]]
+                }]
     print_log("p_func: ", p[0])
 
 def p_paramlist(p):
@@ -46,9 +53,15 @@ def p_param(p):
         	 | INT STAR ID
         	 | FLOAT STAR ID'''
     if len(p) == 4:
-        p[0] = ["id", p[1] + p[2], p[3]]
+        p[0] = ["id", {
+                    "type": p[1] + p[2],
+                    "name": p[3]
+                    }]
     elif len(p) == 3:
-        p[0] = ["id", p[1], p[2]]
+        p[0] = ["id", {
+                    "type": p[1],
+                    "name": p[2]
+                }]
     elif len(p) == 2:
         p[0] = p[1]
     else:
@@ -90,9 +103,17 @@ def p_declare(p):
                | INT STAR declarelist
                | FLOAT STAR declarelist'''
     if len(p) == 4:
-        p[0] = ["declare", p[1] + p[2], p[3], p.lineno(1)]
+        p[0] = ["declare", {
+                    "type": p[1] + p[2],
+                    "vars": p[3],
+                    "lineno": p.lineno(1)
+                }]
     elif len(p) == 3:
-        p[0] = ["declare", p[1], p[2], p.lineno(1)]
+        p[0] = ["declare", {
+                    "type": p[1],
+                    "vars": p[2],
+                    "lineno": p.lineno(1)
+                }]
     print_log("p_declare: ", p[0])
 
 def p_declarelist(p):
@@ -105,17 +126,27 @@ def p_declarelist(p):
 
 def p_assign(p):
     'assign : id ASSIGN expression'
-    p[0] = ["assign", p[1], p[3], p.lineno(2)]
+    p[0] = ["assign", {
+                "var": p[1],
+                "expr": p[3],
+                "lineno": p.lineno(2)
+            }]
     print_log("p_assign: ", p[0])
 
 def p_increment_id_inc(p):
     '''increment : id INCREMENT'''
-    p[0] = ["increment", p[1], p.lineno(1)]
+    p[0] = ["increment", {
+            "var": p[1],
+            "lineno": p.lineno(1)
+        }]
     print_log("p_increment: ", p[0])
 
 def p_increment_inc_id(p):
     '''increment : INCREMENT id'''
-    p[0] = ["increment", p[2], p.lineno(1)]
+    p[0] = ["increment", {
+            "var": p[2],
+            "lineno": p.lineno(1)
+        }]
     print_log("p_increment: ", p[0])
 
 def p_functcall(p):
@@ -125,9 +156,16 @@ def p_functcall(p):
     for arg in p[3]:
         if p[3].index(arg) is not 0:
             func_str += ','
-        func_str += arg[-2]
-        func_arg_list += arg[-3]
-    p[0] = ["functcall", p[1], ["args", p[3]], func_arg_list, func_str, p.lineno(1)]
+        func_str += arg[1]["str"]
+        func_arg_list += arg[1]["arg_list"]
+    p[0] = ["functcall", {
+                "callee": p[1],
+                "args": p[3],
+                "arg_list": func_arg_list,
+                "str": func_str,
+                "lineno": p.lineno(1)
+            }]
+#    p[0] = ["functcall", p[1], ["args", p[3]], func_arg_list, func_str, p.lineno(1)]
     print_log("p_functcall: ", p[0])
 
 def p_arglist(p):
@@ -157,7 +195,11 @@ def p_arg_string(p):
 
 def p_string(p):
     '''string : STRING'''
-    p[0] = ["string", [], p[1], p.lineno(1)]
+    p[0] = ["string", {
+        "arg_list": [],
+        "str": p[1],
+        "lineno": p.lineno(1)
+    }]
 
 def p_arg_empty(p):
     '''arg : empty'''
@@ -168,9 +210,15 @@ def p_return(p):
     '''return : RETURN expression
               | RETURN'''
     if len(p) == 2:
-        p[0] = ["return", None, p.lineno(1)]
+        p[0] = ["return", {
+                    "value": None,
+                    "lineno": p.lineno(1)
+            }]
     else:
-        p[0] = ["return", p[2], p.lineno(1)]
+        p[0] = ["return", {
+                    "value": p[2],
+                    "lineno": p.lineno(1)
+            }]
     print_log("p_return: ", p[0])
 
 def p_expression(p):
@@ -181,9 +229,16 @@ def p_expression(p):
     if len(p) == 2:
         p[0] = p[1]
     else:
-        expr_arg_list = list(set(p[1][-3]) | set(p[3][-3]))
-        expr_str = p[1][-2] + p[2] + p[3][-2]
-        p[0] = [p[2], p[1], p[3], expr_arg_list, expr_str, p.lineno(1)]
+        expr_arg_list = list(set(p[1][1]["arg_list"]) | set(p[3][1]["arg_list"]))
+        expr_str = p[1][1]["str"] + p[2] + p[3][1]["str"]
+        p[0] = ["expression", {
+            "op": p[2],
+            "lhs": p[1],
+            "rhs": p[3],
+            "arg_list": expr_arg_list,
+            "str": expr_str,
+            "lineno": p.lineno(1)
+        }]
     print_log("p_expression: ", p[0])
 
 def p_term(p):
@@ -191,9 +246,16 @@ def p_term(p):
             | term DIVIDE factor
             | factor'''
     if len(p) == 4:
-        term_arg_list = list(set(p[1][-3]) | set(p[3][-3]))
-        term_str = p[1][-2] + p[2] + p[3][-2]
-        p[0] = [p[2], p[1], p[3], term_arg_list, term_str, p.lineno(1)]
+        term_arg_list = list(set(p[1][1]["arg_list"]) | set(p[3][1]["arg_list"]))
+        term_str = p[1][1]["str"] + p[2] + p[3][1]["str"]
+        p[0] = ["expression", {
+            "op": p[2],
+            "lhs": p[1],
+            "rhs": p[3],
+            "arg_list": term_arg_list,
+            "str": term_str,
+            "lineno": p.lineno(1)
+        }]
     else:
         p[0] = p[1]
     print_log("p_term: ", p[0])
@@ -209,15 +271,31 @@ def p_factor_num(p):
               | LPAREN PLUS NUMBER RPAREN
               | LPAREN MINUS NUMBER RPAREN'''
     if len(p) == 2:
-      p[0] = ["number", p[1], [], str(p[1]), p.lineno(1)]
+        p[0] = ["number", {
+            "value": p[1],
+            "arg_list": [],
+            "str": str(p[1]),
+            "lineno": p.lineno(1)
+        }]
     elif len(p) == 4:
-      p[0] = ["number", p[2], [], str(p[2]), p.lineno(1)]
+        p[0] = ["number", {
+            "value": p[2],
+            "arg_list": [],
+            "str": str(p[2]),
+            "lineno": p.lineno(1)
+        }]
     else:
-      if (p[2] == '+'):
-        num = p[3]
-      elif (p[2] == '-'):
-        num = -p[3]
-      p[0] = ["number", num, [], str(num), p.lineno(1)]
+        if (p[2] == '+'):
+            num = p[3]
+        elif (p[2] == '-'):
+            num = -p[3]
+
+        p[0] = ["number", {
+            "value": num,
+            "arg_list": [],
+            "str": str(num),
+            "lineno": p.lineno(1)
+        }]
 
     print_log("p_factor: ", p[0])
 
@@ -235,33 +313,66 @@ def p_id(p):
     '''id : ID
           | ID LBRACKET expression RBRACKET'''
     if len(p) == 2:
-        p[0] = ["id", p[1], [p[1]], p[1], p.lineno(1)]
+        p[0] = ["id", {
+            "name": p[1],
+            "arg_list": [p[1]],
+            "str": p[1],
+            "lineno": p.lineno(1)
+        }]
     else:
-        array_arg_list = list(set([p[1]]) | set(p[3][-3]))
-        array_str = p[1] + '[' + p[3][-2] + ']'
-        p[0] = ["array", p[1], p[3], array_arg_list, array_str, p.lineno(1)]
+        array_arg_list = list(set([p[1]]) | set(p[3][1]["arg_list"]))
+        array_str = p[1] + '[' + p[3][1]["str"] + ']'
+        p[0] = ["array", {
+            "name": p[1],
+            "index": p[3],
+            "arg_list": array_arg_list,
+            "str": array_str,
+            "lineno": p.lineno(1)
+        }]
     print_log("p_factor: ", p[0])
 
 def p_casting(p):
     '''casting : LPAREN INT RPAREN expression
                | LPAREN FLOAT RPAREN expression'''
-    casting_str = '(' + p[2] + ')'+p[4][-2]
-    p[0] = ["casting", p[2], p[4], p[4][-3], casting_str, p.lineno(1)]
+    casting_str = '(' + p[2] + ')' + p[4][1]["str"]
+
+    p[0] = ["casting", {
+        "type": p[2],
+        "expr": p[4],
+        "arg_list": p[4][1]["arg_list"],
+        "str": casting_str,
+        "lineno": p.lineno(1)
+    }]
     print_log("p_casting: ", p[0])
 
 def p_forloop(p):
     'forloop : FOR LPAREN assign SEMICOLON condition SEMICOLON increment RPAREN lbrace stmtlist rbrace'
-    p[0] = ["for", p[3], p[5], p[7], [p[9]] + p[10] + [p[11]], [p.lineno(1), p[11][1]]]
+    p[0] = ["for", {
+        "assign": p[3],
+        "condition": p[5],
+        "increment": p[7],
+        "stmts": [p[9]] + p[10] + [p[11]],
+        "lineno": [p.lineno(1), p[11][1]],
+    }]
     print_log("p_forloop: ", p[0])
 
 def p_if(p):
     'if : IF LPAREN condition RPAREN lbrace stmtlist rbrace'
-    p[0] = ["if", p[3], [p[5]] + p[6] + [p[7]], [p.lineno(1), p[7][1]]]
+    p[0] = ["if", {
+        "condition": p[3],
+        "stmts": [p[5]] + p[6] + [p[7]],
+        "lineno": [p.lineno(1), p[7][1]]
+    }]
     print_log("p_if: ", p[0])
 
 def p_condition(p):
     'condition : ID cmp expression'
-    p[0] = [p[1], p[2], p[3], p.lineno(1)]
+    p[0] = ["condition", {
+        "var": p[1],
+        "cmp": p[2],
+        "expr": p[3],
+        "lineno": p.lineno(1)
+    }]
     print_log("p_condition: ", p[0])
 
 def p_cmp(p):
@@ -276,11 +387,11 @@ def p_cmp(p):
 
 def p_lbrace(p):
     'lbrace : LBRACE'
-    p[0] = ["{", p.lineno(1)]
+    p[0] = ["{", {"lineno": p.lineno(1)}]
 
 def p_rbrace(p):
     'rbrace : RBRACE'
-    p[0] = ["}", p.lineno(1)]
+    p[0] = ["}", {"lineno": p.lineno(1)}]
 
 
 def p_error(p):
@@ -305,8 +416,8 @@ def get_parser_tree(code):
 parser = None
 
 if __name__ == '__main__':
-    f = open("inputs/input0.c")
+    f = open("inputs/easy.c")
     code = "".join(f.readlines())
     f.close()
 
-    print(parser.parse(code, lexer=lexer))
+    print(get_parser_tree(code))
